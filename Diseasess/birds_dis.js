314dd9b90eadf8d9ac9fd9efc1744961a6b664d8
NIKITA-19 <guptanikita1607@gmail.com>
2021-09-04 20:57:57 +0530

    Added Diseases Files

---
+++ Diseasess/birds_dis.js
@@ -0,0 +1,77 @@
+ function pd_abt_fun() {
+    var abt = document.getElementById("pd_ab");
+    var mabt = document.getElementById("pd_more_abt");
+    if (abt.style.display === "inline") {
+      abt.style.display = "none";
+      mabt.innerHTML = "ABOUT"; 
+    } else {
+      abt.style.display = "inline";
+      mabt.innerHTML = "HIDE ABOUT";
+    }
+  }
+  function pd_tre_fun() {
+    var abt = document.getElementById("pd_tre");
+    var mabt = document.getElementById("pd_more_tre");
+    if (abt.style.display === "inline") {
+      abt.style.display = "none";
+      mabt.innerHTML = "TREATMENT";  
+    } else {
+      abt.style.display = "inline";
+      mabt.innerHTML = "HIDE TREATMENT";
+    }
+  }
+  function pf_abt_fun() {
+    var abt = document.getElementById("pf_ab");
+    var mabt = document.getElementById("pf_more_abt");
+    if (abt.style.display === "inline") {
+      abt.style.display = "none";
+      mabt.innerHTML = "ABOUT"; 
+    } else {
+      abt.style.display = "inline";
+      mabt.innerHTML = "HIDE ABOUT";
+    }
+  }
+  function pf_pre_fun() {
+    var abt = document.getElementById("pf_pre");
+    var mabt = document.getElementById("pf_more_pre");
+    if (abt.style.display === "inline") {
+      abt.style.display = "none";
+      mabt.innerHTML = "PREVENTION"; 
+    } else {
+      abt.style.display = "inline";
+      mabt.innerHTML = "HIDE PREVENTION";
+    }
+  }
+  function pf_tre_fun() {
+    var abt = document.getElementById("pf_tre");
+    var mabt = document.getElementById("pf_more_tre");
+    if (abt.style.display === "inline") {
+      abt.style.display = "none";
+      mabt.innerHTML = "TREATMENT"; 
+    } else {
+      abt.style.display = "inline";
+      mabt.innerHTML = "HIDE TREATMENT";
+    }
+  }
+  function po_abt_fun() {
+    var abt = document.getElementById("po_ab");
+    var mabt = document.getElementById("po_more_abt");
+    if (abt.style.display === "inline") {
+      abt.style.display = "none";
+      mabt.innerHTML = "ABOUT"; 
+    } else {
+      abt.style.display = "inline";
+      mabt.innerHTML = "HIDE ABOUT";
+    }
+  }
+  function po_tre_fun() {
+    var abt = document.getElementById("po_tre");
+    var mabt = document.getElementById("po_more_tre");
+    if (abt.style.display === "inline") {
+      abt.style.display = "none";
+      mabt.innerHTML = "TREATMENT";  
+    } else {
+      abt.style.display = "inline";
+      mabt.innerHTML = "HIDE TREATMENT";
+    }
+  }
---
+++ Diseasess/cats_dis.js
@@ -0,0 +1,135 @@
+function can_abt_fun() {
+    var aboutt = document.getElementById("can_ab");
+    var more_abtt = document.getElementById("can_more_abt");
+    var disbox= document.getElementById("dis1");
+    if (aboutt.style.display === "inline") {
+      aboutt.style.display = "none";
+      more_abtt.innerHTML = "ABOUT";
+    } else {
+      aboutt.style.display = "inline";
+      more_abtt.innerHTML = "HIDE ABOUT";
+    }
+  } 
+  function can_pre_fun()
+  {
+      var pre = document.getElementById("can_pre");
+      var more_pre = document.getElementById("can_more_pre");
+      if (pre.style.display === "inline") {
+          pre.style.display = "none";
+          more_pre.innerHTML = "PREVENTION";
+        } else {
+          pre.style.display = "inline";
+          more_pre.innerHTML = "HIDE PREVENTION";
+        }
+  } 
+  function can_tre_fun()
+  {
+      var tre = document.getElementById("can_tre");
+      var more_tre = document.getElementById("can_more_tre");
+      if (tre.style.display === "inline") {
+          tre.style.display = "none";
+          more_tre.innerHTML = "TREATMENT"; 
+        } else {
+          tre.style.display = "inline";
+          more_tre.innerHTML = "HIDE TREATMENT";
+        }
+  } 
+  function di_abt_fun() {
+    var abt = document.getElementById("di_ab");
+    var mabt = document.getElementById("di_more_abt");
+    if (abt.style.display === "inline") {
+      abt.style.display = "none";
+      mabt.innerHTML = "ABOUT"; 
+    } else {
+      abt.style.display = "inline";
+      mabt.innerHTML = "HIDE ABOUT";
+    }
+  }
+  function di_pre_fun() {
+    var abt = document.getElementById("di_pre");
+    var mabt = document.getElementById("di_more_pre");
+    if (abt.style.display === "inline") {
+      abt.style.display = "none";
+      mabt.innerHTML = "PREVENTION"; 
+    } else {
+      abt.style.display = "inline";
+      mabt.innerHTML = "HIDE PREVENTION";
+    }
+  }
+  function di_tre_fun() {
+    var abt = document.getElementById("di_tre");
+    var mabt = document.getElementById("di_more_tre");
+    if (abt.style.display === "inline") {
+      abt.style.display = "none";
+      mabt.innerHTML = "TREATMENT"; 
+    } else {
+      abt.style.display = "inline";
+      mabt.innerHTML = "HIDE TREATMENT";
+    }
+  }
+  function wo_abt_fun() {
+    var abt = document.getElementById("wo_ab");
+    var mabt = document.getElementById("wo_more_abt");
+    if (abt.style.display === "inline") {
+      abt.style.display = "none";
+      mabt.innerHTML = "ABOUT"; 
+    } else {
+      abt.style.display = "inline";
+      mabt.innerHTML = "HIDE ABOUT";
+    }
+  }
+  function wo_pre_fun() {
+    var abt = document.getElementById("wo_pre");
+    var mabt = document.getElementById("wo_more_pre");
+    if (abt.style.display === "inline") {
+      abt.style.display = "none";
+      mabt.innerHTML = "PREVENTION"; 
+    } else {
+      abt.style.display = "inline";
+      mabt.innerHTML = "HIDE PREVENTION";
+    }
+  }
+  function wo_tre_fun() {
+    var abt = document.getElementById("wo_tre");
+    var mabt = document.getElementById("wo_more_tre");
+    if (abt.style.display === "inline") {
+      abt.style.display = "none";
+      mabt.innerHTML = "TREATMENT"; 
+    } else {
+      abt.style.display = "inline";
+      mabt.innerHTML = "HIDE TREATMENT";
+    }
+  }
+  function fiv_abt_fun() {
+    var abt = document.getElementById("fiv_ab");
+    var mabt = document.getElementById("fiv_more_abt");
+    if (abt.style.display === "inline") {
+      abt.style.display = "none";
+      mabt.innerHTML = "ABOUT"; 
+    } else {
+      abt.style.display = "inline";
+      mabt.innerHTML = "HIDE ABOUT";
+    }
+  }
+  function fiv_pre_fun() {
+    var abt = document.getElementById("fiv_pre");
+    var mabt = document.getElementById("fiv_more_pre");
+    if (abt.style.display === "inline") {
+      abt.style.display = "none";
+      mabt.innerHTML = "PREVENTION"; 
+    } else {
+      abt.style.display = "inline";
+      mabt.innerHTML = "HIDE PREVENTION";
+    }
+  }
+  function fiv_tre_fun() {
+    var abt = document.getElementById("fiv_tre");
+    var mabt = document.getElementById("fiv_more_tre");
+    if (abt.style.display === "inline") {
+      abt.style.display = "none";
+      mabt.innerHTML = "TREATMENT"; 
+    } else {
+      abt.style.display = "inline";
+      mabt.innerHTML = "HIDE TREATMENT";
+    }
+  }
